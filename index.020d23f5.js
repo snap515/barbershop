9299a99e527f9dcbe8624142c38b0f58dea153dd
snap515 <77937504+snap515@users.noreply.github.com>
2022-12-09 12:04:11 +0000

    Deploying to gh-pages from @ snap515/barbershop@3dce19f7a907adbbcf2c47fdc9b1196c16853caa 🚀

---
+++ index.020d23f5.js
@@ -0,0 +1,2 @@
+!function(n,o,c){const t=c(n,o);console.log(t)}(2,14,(function(n,o){return n+o}));
+//# sourceMappingURL=index.020d23f5.js.map
---
+++ index.cfb35fe7.js
@@ -0,0 +1,2 @@
+!function(){var n;n=function(n,o){return n+o}(2,14),console.log(n)}();
+//# sourceMappingURL=index.cfb35fe7.js.map
