d68d5335ec916086249b8507045c8a62e3f56699
Anton Pedan <77937504+snap515@users.noreply.github.com>
2022-12-21 00:25:34 +0100

    завершил 4 модуль

--- src/index.js
+++ src/index.js
@@ -289,28 +289,189 @@
 
 // getUserNames(users);
 
-const books = [
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction', 'mysticism'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism', 'adventure'],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genre, index, array) => array.indexOf(genre) === index
+// );
+
+// console.log(allGenres);
+// console.log(uniqueGenres);
+
+const users = [
   {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    genres: ['adventure', 'history'],
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37,
   },
   {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    genres: ['fiction', 'mysticism'],
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34,
   },
   {
-    title: 'Redder Than Blood',
-    author: 'Tanith Lee',
-    genres: ['horror', 'mysticism', 'adventure'],
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39,
   },
 ];
-// Change code below this line
-const allGenres = books.flatMap(book => book.genres);
-const uniqueGenres = allGenres.filter(
-  (genre, index, array) => array.indexOf(genre) === index
-);
 
-console.log(allGenres);
-console.log(uniqueGenres);
+// const getFriends = users => {
+//   const allFriends = users.flatMap(user => user.friends);
+//   console.log(allFriends);
+//   const filterFriends = allFriends.filter(
+//     (friend, index, array) => array.indexOf(friend) === index
+//   );
+//   console.log(filterFriends);
+//   return filterFriends;
+// };
+
+// getFriends(users);
+
+// const getActiveUsers = users => {
+//   const activeUsers = users.filter(user => user.isActive);
+//   console.table(activeUsers);
+//   return activeUsers;
+// };
+
+// getActiveUsers(users);
+
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+// const totalAveragePlaytimePerGame = players.reduce((totalTime, player) => {
+//   return totalTime + player.playtime / player.gamesPlayed;
+// }, 0);
+// console.log(totalAveragePlaytimePerGame);
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+
+// const names = books
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(book => book.author)
+//   .sort((prevAuthor, nextAuthor) => prevAuthor.localeCompare(nextAuthor));
+// console.log(names);
+
+// const getSortedFriends = users => {
+//   const sortFriends = users
+//     .map(user => user.name)
+//     .filter((name, index, array) => array.indexOf(name) === index)
+//     .sort((prevName, nextName) => prevName.localeCompare(nextName));
+//   console.log(sortFriends);
+//   return sortFriends;
+// };
+
+// getSortedFriends(users);
+
+//4 модуль 48 из 48
+const getTotalBalanceByGender = (users, gender) => {
+  const filterByGender = users.filter(user => user.gender === gender);
+  console.log(`отфильтровано по ${gender} полу:`, filterByGender);
+  const totalBalance = filterByGender.reduce(
+    (totalBalance, user) => totalBalance + user.balance,
+    0
+  );
+  console.log(`общий баланс у ${gender} = `, totalBalance);
+};
+
+getTotalBalanceByGender(users, 'male');
+getTotalBalanceByGender(users, 'female');
