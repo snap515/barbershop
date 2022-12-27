8733bc7457276d6d88e2ca181c11ce9b4373d3a7
snap515 <77937504+snap515@users.noreply.github.com>
2022-12-27 11:58:47 +0000

    Deploying to gh-pages from @ snap515/barbershop@8375af878736b4243fdabb58cb7c54c7ef1b2154 🚀

--- index.2e29f382.js
+++
@@ -1,2 +0,0 @@
-!function(o){let c=0;o.forEach((o=>{o[0]>o[2]?c+=3:o[0]===o[2]&&(c+=1)})),console.log(c)}(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]);
-//# sourceMappingURL=index.2e29f382.js.map
---
+++ index.8fac24e7.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=index.8fac24e7.js.map
