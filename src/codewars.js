6274bc327f4eb4e1741d45e419f0e3c53c657981
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-03-22 15:27:55 +0100

    Update codewars.js

--- src/codewars.js
+++ src/codewars.js
@@ -310,3 +310,54 @@
 
 // console.log(animal({ name: 'dog', legs: 4, color: 'white' }));
 // animal({ name: 'dog', legs: 4, color: 'white' });
+
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// function createPhoneNumber(numbers) {
+//   let numbStr = numbers.join('');
+//   return (
+//     '(' +
+//     numbStr[0] +
+//     numbStr[1] +
+//     numbStr[2] +
+//     ')' +
+//     ' ' +
+//     numbStr[3] +
+//     numbStr[4] +
+//     numbStr[5] +
+//     '-' +
+//     numbStr[6] +
+//     numbStr[7] +
+//     numbStr[8] +
+//     numbStr[9]
+//   );
+// }
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+function countSmileys(arr) {
+  let counter = 0;
+  arr.forEach(element => {
+    if (
+      (element.includes(':') || element.includes(';')) &&
+      (element.includes('D') || element.includes(')'))
+    ) {
+      counter += 1;
+    }
+  });
+
+  console.log(counter);
+}
+
+countSmileys([';)', ':D', ';(', ';D']);
