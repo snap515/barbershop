0417c5ec5850c3aa80bcfb4435a81f084bd832f8
snap515 <77937504+snap515@users.noreply.github.com>
2023-05-12 12:03:01 +0000

    Deploying to gh-pages from @ snap515/barbershop@5c3278992a67aea97d02375feccc7b3cea914607 🚀

--- index.8e58b2b4.js
+++
@@ -1,2 +0,0 @@
-function l(l){const e=[];for(let o of l){if(e.length>=3)break;"Hello Kitty"!==o&&"Barbie doll"!==o||e.push(o)}return console.log(e),e}l(["Mickey Mouse","Hello Kitty","Barbie doll","Krasavica"]),l(["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]);
-//# sourceMappingURL=index.8e58b2b4.js.map
