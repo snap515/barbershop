09b4820bd842e8f26ee3eb690431718f86c97ca5
snap515 <77937504+snap515@users.noreply.github.com>
2023-05-17 18:38:18 +0000

    Deploying to gh-pages from @ snap515/barbershop@90effb30840294f83afea1e7e7b2c072323e6cfe 🚀

--- index.712cc85c.js
+++
@@ -1,2 +0,0 @@
-function o(o){const e=[];for(let n in o)5===n.length&&e.push(n),5===o[n].length&&e.push(o[n]);return e}console.log(o({Our:"earth",is:"a",beautyful:"world"})),console.log(o({Ihave:"enough",money:"to",buy:"a",car:"model"}));
-//# sourceMappingURL=index.712cc85c.js.map
