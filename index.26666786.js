f33ffd72651546d34dcc03c6ed7ecfcb086d2141
snap515 <77937504+snap515@users.noreply.github.com>
2023-05-29 10:41:59 +0000

    Deploying to gh-pages from @ snap515/barbershop@a5c145e352ba6156a3d128de1eb2d26b9d3ff5e6 🚀

--- index.26666786.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(e,n){for(var r=n.length,t=e.split("").reverse().join(""),i=n.split("").reverse().join(""),c=0;c<r;c+=1)if(t[c]!==i[c])return!1;return!0}function e(e,n){return e.endsWith(n)}e("abcde","cde"),e("abcde","abc")}();
-//# sourceMappingURL=index.26666786.js.map
--- index.d00784a6.js
+++
@@ -1,2 +0,0 @@
-function e(e,n){const r=n.length,t=e.split("").reverse().join(""),i=n.split("").reverse().join("");for(let e=0;e<r;e+=1)if(t[e]!==i[e])return!1;return!0}function e(e,n){return e.endsWith(n)}e("abcde","cde"),e("abcde","abc");
-//# sourceMappingURL=index.d00784a6.js.map
