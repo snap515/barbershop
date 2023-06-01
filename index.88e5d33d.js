25322d9f4fb66db0fcd8e9c776c386b001161c1a
snap515 <77937504+snap515@users.noreply.github.com>
2023-06-01 10:16:56 +0000

    Deploying to gh-pages from @ snap515/barbershop@983659f89099e632a353d24fd62c72a84f05d2ed 🚀

--- index.88e5d33d.js
+++
@@ -1,2 +0,0 @@
-const e=document.createElement("ul"),n=document.getElementById("container"),t=["HTML","CSS","Javascript","React","Node"].map((e=>`<li>${e}</li>`)).join("");e.insertAdjacentHTML("beforeEnd",t),n.appendChild(e),console.log("🚀 ~ file: codewars.js:1752 ~ listEl:",e);
-//# sourceMappingURL=index.88e5d33d.js.map
